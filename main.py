05a3e6572b17558197d975e95f5501911441fd5e
juliesigal <90146733+juliesigal@users.noreply.github.com>
2021-12-10 13:24:48 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-# main.py
-from datetime import datetime
 from Car import Car
 from sqlalchemy import text
 from db_cofig import local_session, create_all_entities
@@ -11,7 +9,7 @@ create_all_entities()
 # local_session.commit()
 
 # add a list of cars
-cars_list = [Car(model='mustang', brand='ford'), Car(mosel='500', brand='fiat')]
+cars_list = [Car(model='mustang', brand='ford', year = 1989), Car(model='500', brand='fiat', year = 2009)]
 local_session.add_all(cars_list)
 local_session.commit()
 
@@ -21,4 +19,4 @@ local_session.commit()
 
 # print all cars
 users = local_session.query(Car).all()
-print(users)
+print(cars)
